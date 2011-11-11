3ac99ca7199854bae4cf59b9546fb4b5628094e8
Dem0n13 <Dem0n13.kaluga@gmail.com>
2011-11-11 22:56:52 +0400

    fix ошибки с выводом следующего процесса

--- CourseProject/VisualAdapter.cs
+++ CourseProject/VisualAdapter.cs
@@ -338,7 +338,7 @@ namespace OS
                 EditorPanel.Enabled = false;
             }
 #endif
-            LabelProc.Text = "Следующий процесс: " + TaskManager.Processes[TaskManager.CurrentProcessIndex].ID;
+            LabelProc.Text = "Процесс на выполнение: " + (TaskManager.CurrentProcessIndex == -1 ? "Нет" : TaskManager.Processes[TaskManager.CurrentProcessIndex].ID);
         }
 
 #if UI_REQUEST_EDITOR_ON_MAIN_FORM
