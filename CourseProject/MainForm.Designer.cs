e29c8b4f2f5f3d98a9a4d5afe7d05ce59b0c9201
Dem0n13 <Dem0n13.kaluga@gmail.com>
2011-11-19 23:09:39 +0400

    Формы, шрифты, вариант

--- CourseProject/MainForm.Designer.cs
+++ CourseProject/MainForm.Designer.cs
@@ -70,13 +70,13 @@
             this.Процесс,
             this.Заявка,
             this.Column13});
-            this.DGRequsts.Location = new System.Drawing.Point(142, 37);
+            this.DGRequsts.Location = new System.Drawing.Point(19, 37);
             this.DGRequsts.MultiSelect = false;
             this.DGRequsts.Name = "DGRequsts";
             this.DGRequsts.ReadOnly = true;
             this.DGRequsts.RowHeadersVisible = false;
             this.DGRequsts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DGRequsts.Size = new System.Drawing.Size(660, 246);
+            this.DGRequsts.Size = new System.Drawing.Size(483, 246);
             this.DGRequsts.TabIndex = 5;
             this.DGRequsts.SelectionChanged += new System.EventHandler(this.DGRequsts_SelectionChanged);
             // 
@@ -116,7 +116,7 @@
             this.Column11,
             this.Column12});
             this.PagesInMemory.GridColor = System.Drawing.Color.DimGray;
-            this.PagesInMemory.Location = new System.Drawing.Point(583, 318);
+            this.PagesInMemory.Location = new System.Drawing.Point(766, 373);
             this.PagesInMemory.Name = "PagesInMemory";
             this.PagesInMemory.RowHeadersVisible = false;
             this.PagesInMemory.Size = new System.Drawing.Size(219, 262);
@@ -164,7 +164,7 @@
             this.DescriptionPages.AllowUserToResizeRows = false;
             this.DescriptionPages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DescriptionPages.GridColor = System.Drawing.Color.DimGray;
-            this.DescriptionPages.Location = new System.Drawing.Point(212, 318);
+            this.DescriptionPages.Location = new System.Drawing.Point(354, 373);
             this.DescriptionPages.MultiSelect = false;
             this.DescriptionPages.Name = "DescriptionPages";
             this.DescriptionPages.RowHeadersVisible = false;
@@ -180,7 +180,7 @@
             this.DescriptionTable.AllowUserToResizeRows = false;
             this.DescriptionTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DescriptionTable.GridColor = System.Drawing.Color.DimGray;
-            this.DescriptionTable.Location = new System.Drawing.Point(14, 319);
+            this.DescriptionTable.Location = new System.Drawing.Point(542, 38);
             this.DescriptionTable.Name = "DescriptionTable";
             this.DescriptionTable.RowHeadersVisible = false;
             this.DescriptionTable.Size = new System.Drawing.Size(188, 261);
@@ -189,7 +189,7 @@
             // Catalog
             // 
             this.Catalog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Catalog.Location = new System.Drawing.Point(809, 37);
+            this.Catalog.Location = new System.Drawing.Point(24, 324);
             this.Catalog.Name = "Catalog";
             this.Catalog.Size = new System.Drawing.Size(282, 86);
             this.Catalog.TabIndex = 0;
@@ -197,14 +197,14 @@
             // CellsArray
             // 
             this.CellsArray.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.CellsArray.Location = new System.Drawing.Point(809, 167);
+            this.CellsArray.Location = new System.Drawing.Point(24, 448);
             this.CellsArray.Name = "CellsArray";
-            this.CellsArray.Size = new System.Drawing.Size(282, 413);
+            this.CellsArray.Size = new System.Drawing.Size(282, 218);
             this.CellsArray.TabIndex = 4;
             // 
             // RunStepBtn
             // 
-            this.RunStepBtn.Location = new System.Drawing.Point(14, 37);
+            this.RunStepBtn.Location = new System.Drawing.Point(126, -1);
             this.RunStepBtn.Name = "RunStepBtn";
             this.RunStepBtn.Size = new System.Drawing.Size(113, 34);
             this.RunStepBtn.TabIndex = 0;
@@ -225,38 +225,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(143, 10);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.Size = new System.Drawing.Size(100, 24);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Диспетчер задач";
+            this.label1.Text = "Процессы";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(15, 292);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(543, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(187, 24);
+            this.label2.Size = new System.Drawing.Size(200, 24);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Дескриптор таблиц";
+            this.label2.Text = "Дескрипторы таблиц";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(208, 292);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(350, 347);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(198, 24);
+            this.label3.Size = new System.Drawing.Size(211, 24);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Дескриптор страниц";
+            this.label3.Text = "Дескрипторы страниц";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(579, 292);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(762, 347);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 24);
             this.label4.TabIndex = 9;
@@ -265,8 +265,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(805, 10);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(20, 297);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(82, 24);
             this.label5.TabIndex = 10;
@@ -276,19 +276,19 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(808, 140);
+            this.label6.Location = new System.Drawing.Point(23, 421);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 24);
+            this.label6.Size = new System.Drawing.Size(135, 24);
             this.label6.TabIndex = 11;
-            this.label6.Text = "ВЗУ";
+            this.label6.Text = "Жесткий диск";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(331, 10);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(245, 9);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 20);
+            this.label7.Size = new System.Drawing.Size(60, 24);
             this.label7.TabIndex = 12;
             this.label7.Text = "label7";
             // 
@@ -296,7 +296,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1107, 595);
+            this.ClientSize = new System.Drawing.Size(1015, 689);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -312,9 +312,10 @@
             this.Controls.Add(this.RunStepBtn);
             this.Controls.Add(this.PagesInMemory);
             this.Controls.Add(this.CellsArray);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Курсовой проект ОС";
+            this.Text = "Курсовой проект Рябкова Дмитрия";
             ((System.ComponentModel.ISupportInitialize)(this.DGRequsts)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PagesInMemory)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DescriptionPages)).EndInit();
